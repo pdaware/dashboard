09ac5e4f64d69f6dd374e92cd13d4e5cef266dce
Pritesh <priteshdaware@gmail.com>
2025-01-04 21:59:20 +0530

    left side bar completed

--- src/app/main-wrapper/left-sidebar/left-sidebar.component.ts
+++ src/app/main-wrapper/left-sidebar/left-sidebar.component.ts
@@ -1,11 +1,40 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-left-sidebar',
-  imports: [],
+  imports: [NgFor, NgIf, NgClass, RouterLinkActive, RouterLink],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.scss'
 })
 export class LeftSidebarComponent {
+  linksData: any[] = [
+    {
+      name: 'Dashboard',
+      route: '/dashboard/home'
+    },
+    {
+      name: 'Classes',
+      route: '/dashboard/classes'
+    },
+    {
+      name: 'Resources',
+      route: '/dashboard/resources'
+    },
+    {
+      name: 'Learning plan',
+      route: '/dashboard/learning-plan'
+    },
+    {
+      name: 'Chat',
+      route: '/dashboard/chat',
+      notfication_count: 4,
+    },
+    {
+      name: 'Settings',
+      route: '/dashboard/settings'
+    },
+  ]
 
 }
